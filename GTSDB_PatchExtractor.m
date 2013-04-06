f7578e0d173cadde89689f6cc928350b3a659b15
Poul Sørensen <poulmobil+github@gmail.com>
2013-04-06 14:36:08 +0200

    Hello

--- GTSDB_PatchExtractor.m
+++ GTSDB_PatchExtractor.m
@@ -1,7 +1,7 @@
 %% Deep Learning Data Extractino
 clc;clear all;close all;
 
-NUMBER_OF_SAMPLES = 100000; %
+NUMBER_OF_SAMPLES = 1000000; %
 PATCH_SIZE = 8;
 NUMBER_OF_FILES = 599;
 
@@ -15,13 +15,21 @@ ATTRIBUTE_SIZE = PATCH_SIZE*PATCH_SIZE*3;
 PATCHES = zeros(PATCH_PER_IMAGE*NUMBER_OF_FILES,PATCH_SIZE*PATCH_SIZE*3);
 size(PATCHES)
 PATCH_COUNTER = 0;
-for i = 1:NUMBER_OF_FILES
+fig = figure(1);
+vidObj = VideoWriter('sample1.avi');
+open(vidObj);
+fileno = -1;
+for i = 1:length(Data)
+    if (fileno == Data(i).fileNo)
+        continue;
+    end
+    fileno = Data(i).fileNo;
     tic
-im = imread(sprintf('TrainIJCNN2013/TrainIJCNN2013/%.5d.ppm',Data(i).fileNo));
+    im = imread(sprintf('TrainIJCNN2013/TrainIJCNN2013/%.5d.ppm',fileno));
 
     imsize = size(im);
-    if (0)
-        figure(1);
+    if (1)
+        
         image(im); 
         axis image
     end
@@ -36,22 +44,26 @@ for n = 1:PATCH_PER_IMAGE
     col = floor(SELECTED_PIXEL_INDEXES(n) / imsize(1))+1;
     row = rem(SELECTED_PIXEL_INDEXES(n), imsize(1))+1;
     PATCHES(PATCH_COUNTER,:) = reshape(im(row:row+PATCH_SIZE-1,col:col+PATCH_SIZE-1,:),1,ATTRIBUTE_SIZE);
-    if (0)
+    if (1)
        rectangle('Position',[col,row,8,8],'EdgeColor',[1 0 0]);
+
     end
     
 end
-   
+ if (1)
+          currFrame = getframe;
+       writeVideo(vidObj,currFrame);
+ end
     toc
 end
-
+close(vidObj);
 %a = rectangle('Position',[ Data(i).leftCol Data(i).topRow ...
 %    (Data(i).bottomRow - Data(i).topRow)+1 (Data(i).rightCol - Data(i).leftCol)+1], 'EdgeColor',[1 0 0] )
 % [x y w h])
 %pause
 %end
 %%
-save('PatchesData.gmat','PATCHES')
+save('PatchesData_8_8_1000000.mat','PATCHES')
 
 
 
